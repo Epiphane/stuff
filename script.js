ce22e52166623989ed605facbc03b2d335610a28
zarend <zacharyarend@gmail.com>
2015-10-02 16:11:48 -0700

    captain falcon will tell you commit streaks

---
+++ script.js
@@ -0,0 +1,64 @@
+var Xray = require('x-ray');
+var fs = require('fs');
+var Download = require('download');
+var moment = require('moment');
+var Slack = require('slack-client');
+
+var args = process.argv.slice(2);
+
+var dateStr = moment().subtract(1, 'days').format('YYYY-MM-DD');
+
+var getContribs = function(user) {
+	var xray = new Xray();
+	var url = 'https://github.com/' + user + '?tab=contributions&from=' + dateStr;
+	console.log('url', url);
+	xray(url, '.text-emphasized')
+	(function(error, results) {
+		console.log('contribs', results);
+	})
+}
+
+var getStreak = function(user, message) {
+	var xray = new Xray();
+	xray('https://github.com/' + user, '.contrib-column', [{
+		desc: '.text-muted',
+		val: '.contrib-number'
+	}])
+	(function(error, results) {
+		// console.log('results', results);
+		var desiredThing = results.find(function(thing) {
+			return thing.desc == "Current streak";
+		});
+		if (desiredThing) {
+			var channel = slack.getChannelGroupOrDMByID(message.channel);
+			var str = desiredThing.desc + ' for ' + user + ': ' + desiredThing.val;
+			channel.send(str);
+		} else {
+			consloe.log('fail whale');
+		}
+	})
+}
+
+var githubHandles = ['zarend', 'kyle-piddington', 'lejonmcgowan'];
+
+
+var movebook = JSON.parse(fs.readFileSync('./ignoreme/.movebook', 'utf8'));
+
+var slackToken = movebook.token;
+var slack = new Slack(slackToken, true, true);
+
+slack.on('open', function() {
+	console.log('connected to ' + slack.team.name + ' as @' + slack.self.name);
+});
+
+slack.on('message', function(message) {
+	var body = message.getBody();
+	if (body.match('<@' + slack.self.id + '>:*.*contest.*')) {
+		var str = githubHandles.forEach(function(name) {
+			getStreak(name, message); 
+		})
+	}
+});
+
+slack.login();
+
