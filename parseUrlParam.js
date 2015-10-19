7194fe9c0c6a216f0084c467de9e48a38770e9ba
zarend <zacharyarend@gmail.com>
2015-10-18 23:36:36 -0700

    add error checking to parseUrlParam

--- parseUrlParam.js
+++ parseUrlParam.js
@@ -2,4 +2,9 @@ var url = require('url');
 
 var args = process.argv.slice(2);
 
-console.log(url.parse(args[0], true).query[args[1]]);
+if (args.length < 2) {
+	console.log('usage: node ./parseUrlParam.js url param');
+}
+else {
+	console.log(url.parse(args[0], true).query[args[1]]);
+}
