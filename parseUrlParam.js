ea9077ec6ed34061f253ec47b2aa4e93551438b3
zarend <zacharyarend@gmail.com>
2015-10-19 09:32:08 -0700

    code cleanup

--- parseUrlParam.js
+++ parseUrlParam.js
@@ -6,11 +6,10 @@ if (args.length < 2) {
 	console.error('usage: node ./parseUrlParam.js url param');
 	process.exit(1);
 }
-else {
-	var query = url.parse(args[0], true).query;
-	if (query[args[1]] === undefined) {
-		console.error('does not contain query');
-		process.exit(1);
-	}
-	console.log(query[args[1]]);
+
+var query = url.parse(args[0], true).query;
+if (query[args[1]] === undefined) {
+	console.error('does not contain query');
+	process.exit(1);
 }
+console.log(query[args[1]]);
