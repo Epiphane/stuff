fe66271b8c18cd165f872088f6abbd65009d561d
zarend <zacharyarend@gmail.com>
2015-10-18 23:43:02 -0700

    even better error handling

--- parseUrlParam.js
+++ parseUrlParam.js
@@ -3,8 +3,14 @@ var url = require('url');
 var args = process.argv.slice(2);
 
 if (args.length < 2) {
-	console.log('usage: node ./parseUrlParam.js url param');
+	console.error('usage: node ./parseUrlParam.js url param');
+	process.exit(1);
 }
 else {
-	console.log(url.parse(args[0], true).query[args[1]]);
+	var query = url.parse(args[0], true).query;
+	if (query[args[1]] === undefined) {
+		console.error('does not contain query');
+		process.exit(1);
+	}
+	console.log(query[args[1]]);
 }
