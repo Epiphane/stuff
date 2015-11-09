e9ea5e69eea08583f9b3530475bb02e5f255cb05
zarend <zacharyarend@gmail.com>
2015-11-08 17:00:49 -0800

    improve falcon runner

--- scripts/falconRunner.sh
+++ scripts/falconRunner.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-node ts/captain.falcon.js $(cat ignoreme/.movebook | jq ".\"$1\"" | xargs)
+movebook=$(cat ignoreme/.movebook)
+if [ $# -lt 1 ]; then
+   echo 'usage: scripts/falconRunner.sh teamname...'
+   exit 1
+fi
+
+for team in $@; do
+   node ts/captain.falcon.js $(cat ignoreme/.movebook | jq ".\"$team\"" | xargs) &
+done
